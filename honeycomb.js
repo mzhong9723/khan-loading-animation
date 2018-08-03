8cd2c4e16b698fb98825eb921b3415a2fcec6a98
Mary Zhong <maryzhong@C02V23D4HTD5.local>
2018-08-03 10:55:16 -0700

    hexagon patch animation

---
+++ honeycomb.js
@@ -0,0 +1,17 @@
+var hexagons = document.getElementsByClassName("hexagon");
+var maxDelay = 2;
+var minLength = 1;
+var maxLength = 3;
+
+for (var i = 0; i < hexagons.length; i++) {
+	var randomDelay = Math.random() * maxDelay;
+	hexagons[i].style.animationDelay = randomDelay + "s";
+
+	var randomLength = Math.random() * (maxLength - minLength);
+	randomLength += minLength;
+	hexagons[i].style.animationDuration = randomLength + "s";
+
+	console.log("hexagon" + i);
+	console.log(randomDelay);
+	console.log(randomLength);
+}
